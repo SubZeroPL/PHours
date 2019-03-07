dbe8da55dff8f69960b342a2a6db678e75d27739
SubZero <subzeropl@gmail.com>
2019-03-07 13:14:15 +0100

    Tray icon and minimize to tray

--- src/main/java/org/linkuei/Main.java
+++ src/main/java/org/linkuei/Main.java
@@ -1,6 +1,7 @@
 package org.linkuei;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,21 +11,33 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
 import java.util.Optional;
 
-public class Main extends Application {
+import static java.awt.Toolkit.getDefaultToolkit;
+
+public class Main extends Application implements MouseListener {
+
+    private Stage stage;
+    private TrayIcon trayIcon;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    private static void onClose(WindowEvent windowEvent) {
+    private void onClose(WindowEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setHeaderText("Really close?");
         Optional<ButtonType> res = alert.showAndWait();
         if (res.isPresent() && res.get().getButtonData().isCancelButton())
-            windowEvent.consume();
+            event.consume();
         else {
+            Platform.setImplicitExit(true);
             if (HoursData.getInstance().isOvertime()) {
                 alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setHeaderText("Add overtime to hours?");
@@ -34,22 +47,105 @@ public class Main extends Application {
                 }
             }
             HoursData.getInstance().save();
+            SystemTray tray = SystemTray.getSystemTray();
+            tray.remove(trayIcon);
         }
+    }
+
+    private void onMinimize(Boolean minimized) {
+        if (minimized)
+            stage.hide();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        Platform.setImplicitExit(false);
+
         HoursData.getInstance().load();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainForm.fxml"));
         Parent root = loader.load();
         ((Controller) loader.getController()).init();
         stage.setTitle("Job");
-        stage.setOnCloseRequest(Main::onClose);
+        stage.setOnCloseRequest(this::onClose);
+        stage.iconifiedProperty().addListener((observable, oldValue, minimized) -> this.onMinimize(minimized));
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_16.png")));
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_48.png")));
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
+
+        // sets up the tray icon (using awt code run on the swing thread).
+        SwingUtilities.invokeLater(this::addAppToTray);
+    }
+
+    /**
+     * Sets up a system tray icon for the application.
+     */
+    private void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            getDefaultToolkit();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                Platform.exit();
+            }
+
+            // set up a system tray icon.
+            SystemTray tray = SystemTray.getSystemTray();
+            java.awt.Image image = ImageIO.read(getClass().getResource("icon_16.png"));
+            trayIcon = new TrayIcon(image);
+            trayIcon.setToolTip("PHours");
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addMouseListener(this);
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (AWTException | IOException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the application stage and ensures that it is brought ot the front of all stages.
+     */
+    private void showStage() {
+        if (stage != null && !stage.isShowing()) {
+            stage.show();
+            stage.toFront();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            Platform.runLater(this::showStage);
+            e.consume();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // not implemented
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // not implemented
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("mouseEntered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("mouseExited");
     }
 }
