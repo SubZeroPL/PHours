7af197d8eb3259e4f64985788a3bbf583f1787ac
SubZeroPL <subzeropl@gmail.com>
2020-11-18 13:49:32 +0100

    Update get_upload_url.sh

--- .github/workflows/get_upload_url.sh
+++ .github/workflows/get_upload_url.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-upload_url=$(curl -s -H "Accept: application/vnd.github.v3.text" https://api.github.com/repos/SubZeroPL/PHours/releases/tags/v1.0-pre | grep -Po '"upload_url": ".*"' | sed 's/\"//g' | sed 's/upload_url: //')
+upload_url=$(curl -s -H "Accept: application/vnd.github.v3.text" https://api.github.com/repos/SubZeroPL/PHours/releases/tags/$tag_name | grep -Po '"upload_url": ".*"' | sed 's/\"//g' | sed 's/upload_url: //')
 echo $upload_url
